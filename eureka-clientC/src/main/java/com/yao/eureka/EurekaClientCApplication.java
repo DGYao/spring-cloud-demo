b984ce7fee523739b00790a18d73eaa28d8c1ab0
e <you@example.com>
2019-05-19 18:16:02 +0800

    抽取zipkin配置到公共配置文件

--- eureka-clientC/src/main/java/com/yao/eureka/EurekaClientCApplication.java
+++ eureka-clientC/src/main/java/com/yao/eureka/EurekaClientCApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableEurekaClient
 public class EurekaClientCApplication {
 
     public static void main(String[] args) {
